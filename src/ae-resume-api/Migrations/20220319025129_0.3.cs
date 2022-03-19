f6868d09034dc1cf892b99c58e0b2e48008c0845
Sean Bocirnea <sean@aetheris.ca>
2022-03-18 19:59:47 -0700

    Add 0.3 migration

---
+++ src/ae-resume-api/Migrations/20220319025129_0.3.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ae_resume_api.Migrations
+{
+    public partial class _03 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Workspace",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ResumeName",
+                table: "Sector",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmployeeName",
+                table: "Resume",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Workspace");
+
+            migrationBuilder.DropColumn(
+                name: "ResumeName",
+                table: "Sector");
+
+            migrationBuilder.DropColumn(
+                name: "EmployeeName",
+                table: "Resume");
+        }
+    }
+}
